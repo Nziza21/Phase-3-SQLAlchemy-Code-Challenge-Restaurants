1f7e2fd6362bfe69fabda41e3e3960bda2f9f8ff
Nziza Samuel <nziza.samuel@student.moringaschool.com>
2023-12-11 00:07:53 +0300

    added

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 # config.py
 class Config:
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///../database.db'  # Adjust based on your database choice
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///../database.db' 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = 'your_secret_key'
